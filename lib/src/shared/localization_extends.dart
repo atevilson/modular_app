a204fea6523b08a3198007b6873fda7cf5fde72d
atevilson <atevilson.araujo@atak.com.br>
2024-06-19 10:43:34 -0300

    variable naming adjustment

--- lib/src/shared/localization_extends.dart
+++ lib/src/shared/localization_extends.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'localization/translate_app.dart';
 
 extension LocalizationExtends on String {
-  String localizacao(BuildContext context) {
+  String locates(BuildContext context) {
     return TranslateApp(context).text(this);
   }
 }
