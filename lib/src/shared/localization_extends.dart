a9d0078e8980744e950466f64b8e16d8ea97ac68
atevilson <freitas.atevilson@gmail.com>
2024-07-01 23:52:45 -0300

    remove

--- lib/src/shared/localization_extends.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'localization/translate_app.dart';
-
-extension LocalizationExtends on String {
-  String locates(BuildContext context) {
-    return TranslateApp(context).text(this);
-  }
-}
