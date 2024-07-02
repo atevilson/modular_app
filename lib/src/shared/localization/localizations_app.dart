cfa22c93737cb55bc48b6d45ed4c3056512f18bc
atevilson <freitas.atevilson@gmail.com>
2024-07-01 23:54:42 -0300

    ajuste para alternar localização via botão na home

--- lib/src/shared/localization/localizations_app.dart
+++ lib/src/shared/localization/localizations_app.dart
@@ -5,12 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class LocalizationsApp {
-  final Locale locale;
+  Locale locale;
   Map<String, String>? localizationString;
 
   LocalizationsApp(this.locale, {this.localizationString});
 
-  Future<bool> load() async {
+  Future<bool> load([Locale? newLocale]) async {
+    if (newLocale != null) {
+      locale = newLocale;
+    }
     String jsonString =
         await rootBundle.loadString('assets/lang/${locale.languageCode}.json');
     Map<String, dynamic> jsonMap = json.decode(jsonString);
@@ -21,13 +24,21 @@ class LocalizationsApp {
     return true;
   }
 
-  String? translate(String key) {
-    return localizationString?[key];
+  String translate(String key) {
+    return localizationString?[key] ?? '[$key]';
   }
+
+  String get appBarTitleHome => translate("appBarTitleHome");
+  String get textTitulo => translate("textTitulo");
+  String get textHome => translate("textHome");
+  String get bottomLogin => translate("bottomLogin");
+  String get bottonBack => translate("bottonBack");
+  String get appBarTitleLogin => translate("appBarTitleLogin");
+  String get appBarTitleProduct => translate("appBarTitleProduct");
 
   static LocalizationsApp of(BuildContext context) {
     return Localizations.of(context, LocalizationsApp);
   }
 
-  static const LocalizationsDelegate delegate = LocalizationAppDelegate();
+  static const LocalizationsDelegate<LocalizationsApp> delegate = LocalizationAppDelegate();
 }
