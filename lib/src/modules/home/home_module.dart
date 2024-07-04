bedb24833229b104325c3f36f6427c30f08c14e1
atevilson <freitas.atevilson@gmail.com>
2024-07-04 13:01:56 -0300

    remove

--- lib/src/modules/home/home_module.dart
+++
@@ -1,9 +0,0 @@
-import 'package:app_modular/src/modules/home/views/home_page.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-
-class HomeModule extends Module {
-  @override
-  void routes(RouteManager r) {
-    r.child('/', child: (_) => const HomePage());
-  }
-}
