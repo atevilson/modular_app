971f6ddd9ac230e29a88edb959778f6f22135666
atevilson <freitas.atevilson@gmail.com>
2024-07-04 13:04:30 -0300

    ajuste path common_dependencies

--- lib/src/bloc/localization_bloc.dart
+++ lib/src/bloc/localization_bloc.dart
@@ -1,10 +1,11 @@
 
 import 'package:app_modular/src/bloc/localization_event_bloc.dart';
 import 'package:app_modular/src/bloc/localization_state_bloc.dart';
-import 'package:app_modular/src/shared/localization/localizations_app.dart';
+import 'package:app_modular/src/modules/commons_dependencies/lib/commons_dependencies.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
+
+
 
 class LocalizationBloc extends Bloc<LocalizationEvent, LocalizationState> {
   LocalizationBloc() : super(const LocalizationState(Locale('pt', 'BR'))) {
