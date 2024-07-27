968c175dc33a69051ee935b12c1fb2a172848ead
atevilson <freitas.atevilson@gmail.com>
2024-07-27 19:37:28 -0300

    add module common e adicionando shared de localizações

---
+++ lib/modules/common/lib/main.dart
@@ -0,0 +1,3 @@
+
+export 'package:common/shared/localization/localization_app_delegate.dart';
+export 'package:common/shared/localization/localizations_app.dart';
---
+++ lib/modules/common/lib/shared/localization/localizations_app.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:common/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LocalizationsApp {
+  Locale locale;
+  Map<String, String> localizationString = {};
+
+  LocalizationsApp(this.locale);
+
+  Future<bool> load([Locale? newLocale]) async {
+    if (newLocale != null) {
+      locale = newLocale;
+    }
+
+    List<String> moduleGlobalAssets = [
+      'packages/auth/assets/lang',
+      'packages/home/assets/lang',
+      'packages/product/assets/lang',
+    ];
+
+    for (String path in moduleGlobalAssets) {
+      try {
+        String jsonString =
+            await rootBundle.loadString('$path/${locale.languageCode}.json');
+        Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+        jsonMap.forEach((key, value) {
+          localizationString[key] = value.toString();
+        });
+      } catch (e) {
+        throw("caminho não localizado $path: $e");
+      }
+    }
+
+    return true;
+  }
+
+  String translate(String key) {
+    return localizationString[key] ?? '[$key]';
+  }
+
+  String get appBarTitleHome => translate("appBarTitleHome");
+  String get textTitulo => translate("textTitulo");
+  String get textHome => translate("textHome");
+  String get bottomLogin => translate("bottomLogin");
+  String get bottonBack => translate("bottonBack");
+  String get appBarTitleLogin => translate("appBarTitleLogin");
+  String get appBarTitleProduct => translate("appBarTitleProduct");
+
+  static LocalizationsApp of(BuildContext context) {
+    return Localizations.of(context, LocalizationsApp);
+  }
+
+  static const LocalizationsDelegate<LocalizationsApp> delegate = LocalizationAppDelegate();
+}
