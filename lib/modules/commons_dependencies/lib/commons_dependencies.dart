7e20271bb2e81338688fe227c8044d8e4479e741
atevilson <freitas.atevilson@gmail.com>
2024-07-27 20:05:28 -0300

    export equatable

--- lib/modules/commons_dependencies/lib/commons_dependencies.dart
+++ lib/modules/commons_dependencies/lib/commons_dependencies.dart
@@ -3,3 +3,4 @@ export 'package:get_it/get_it.dart' hide Disposable;
 export 'package:flutter_modular/flutter_modular.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
+export 'package:equatable/equatable.dart';
