7298291b8936bda468b7db970ed198011a62f7e7
atevilson <freitas.atevilson@gmail.com>
2024-07-27 22:09:09 -0300

    add bloc provider para fornecer o bloc para todos widgets e bloc builder para atualizar as telas com a troca do idioma no botão

--- lib/modules/core/lib/app/app_widget.dart
+++ lib/modules/core/lib/app/app_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:commons_dependencies/commons_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:common/main.dart';
@@ -8,7 +7,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await injectorDependence();
 
-  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+  runApp(ModularApp(module: AppModule(), child: AppWidget()));
 }
 
 Future<void> injectorDependence() async {
@@ -17,32 +16,45 @@ Future<void> injectorDependence() async {
   await localizationsApp.load();
 
   GetIt.I.registerLazySingleton<LocalizationsApp>(() => localizationsApp);
+  GetIt.I.registerLazySingleton<LocalizationBloc>(() => LocalizationBloc());
 }
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({super.key});
+  final LocalizationBloc bloc = GetIt.I<LocalizationBloc>();
+  AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'App modular',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-            foregroundColor: Colors.white, color: Colors.black),
-        primaryColor: Colors.blue,
+    return BlocProvider(
+      create: (context) => bloc,
+      child: BlocBuilder<LocalizationBloc, LocalizationState>(
+        builder: (context, state) {
+
+          return MaterialApp.router(
+            title: 'App modular',
+            theme: ThemeData(
+              appBarTheme: const AppBarTheme(
+                foregroundColor: Colors.white,
+                color: Colors.black,
+              ),
+              primaryColor: Colors.blue,
+            ),
+            locale: state.locale,
+            supportedLocales: const [
+              Locale("pt", "BR"),
+              Locale("en", "US"),
+              Locale("es", "ES"),
+            ],
+            localizationsDelegates: const [
+              LocalizationsApp.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            routerConfig: Modular.routerConfig,
+          );
+        },
       ),
-      supportedLocales: const [
-        Locale("pt", "BR"),
-        Locale("en", "US"),
-        Locale("es", "ES")
-      ],
-      localizationsDelegates: const [
-        LocalizationsApp.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      routerConfig: Modular.routerConfig,
     );
   }
 }
