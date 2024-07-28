71f34b30f78b3f369241730f0c84ef23c9c457a1
atevilson <freitas.atevilson@gmail.com>
2024-07-28 19:16:51 -0300

    add path injecao

--- lib/modules/common/lib/main.dart
+++ lib/modules/common/lib/main.dart
@@ -4,3 +4,4 @@ export 'package:common/shared/localization/localizations_app.dart';
 export 'package:common/bloc/localization_bloc.dart';
 export 'package:common/bloc/localization_event_bloc.dart';
 export 'package:common/bloc/localization_state_bloc.dart';
+export 'package:common/injector_dependece/injector_dependence.dart';
